cfdcb45c06a4ca6957eb9cc1fb4f429b2429cdf6
Damien Irving <irving.damien@gmail.com>
2017-09-06 11:55:15 +1000

    Switched to seaborn

--- code/bar_plot.py
+++ code/bar_plot.py
@@ -11,22 +11,28 @@ def main(inargs):
     assert len(inargs.labels) == len(inargs.frequencies)
 
     # Read data into dataframe
-    data = list(zip(inargs.labels, inargs.frequencies))
+    labels = [x.replace('_', ' ') for x in inargs.labels]
+    data = list(zip(labels, inargs.frequencies))
     headers = ['label', 'frequency']
     df = pd.DataFrame.from_records(data, columns=headers)
     
     # Create plot
     fig, ax = plt.subplots()
-    sns.barplot(x="frequency", y="label", data=df, palette="BuGn_d")
+    if inargs.palette == 'qualitative':
+        palette = "Set3"
+    elif inargs.palette == 'sequential':
+        palette = "BuGn_d"
+    
+    sns.barplot(x="frequency", y="label", data=df, palette=palette)
     ax.set_ylabel("")
     sns.despine(left=True, top=True, right=True)
 
     if inargs.title:
         ax.set_title(inargs.title.replace('_',' '))
 
-    plt.savefig(inargs.outfile)
-    
+    plt.savefig(inargs.outfile, bbox_inches='tight')
 
+    
 if __name__ == '__main__':
 
     extra_info =""" 
@@ -46,6 +52,9 @@ author:
                         help="bar labels (will appear top to bottom")
     parser.add_argument("--frequencies", type=int, nargs='*', required=True,
                         help="frequencies")
+    
+    parser.add_argument("--palette", type=str, choices=('sequential', 'qualitative'), default='qualitative',
+                        help="Colors for the bars")
 
     parser.add_argument("--title", type=str, default=None,
                         help="plot title")
